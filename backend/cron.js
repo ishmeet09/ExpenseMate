873a3e3f979397c2af72cc7b7fe06e382607873e
ishmeet09 <108580632+ishmeet09@users.noreply.github.com>
2024-07-15 00:32:14 +0530

    Update cron.js

--- backend/cron.js
+++ backend/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://graphql-crash-course.onrender.com";
+const URL = "https://expensemate-2d5s.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
